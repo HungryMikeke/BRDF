213ed954e29a2a75bc5b262d4662de32a3bcd8d8
HungryMikeke <hungrymikeke@gmail.com>
2014-06-23 02:44:21 +0800

    Add Shader For BRDF

--- js/main_BRDF.js
+++ js/main_BRDF.js
@@ -25,7 +25,8 @@ require(["jquery", "gl-matrix", "modernizr", "model", "shader", "timer"], functi
 	var g_WebGLContext = undefined;
 	var g_Shader = undefined;
 	var g_Model = undefined;
-
+	
+	// 全局参数
 	var g_CanvasAspectRatio = 1.0;
 	var g_AnimationKey = 0.0;
 
@@ -48,7 +49,7 @@ require(["jquery", "gl-matrix", "modernizr", "model", "shader", "timer"], functi
 	// 函数
 	var initWebGLContext = function(canvasID) {
 		
-		var webGLContext;
+		var webGLContext = undefined;
 		
 		var canvas = document.getElementById(canvasID);
 		if (undefined != canvas) 
@@ -117,12 +118,12 @@ require(["jquery", "gl-matrix", "modernizr", "model", "shader", "timer"], functi
 		var program = g_Shader.getGLProgramID();
 
 		// Init Light Direction Slot
-		g_LightDirVecUniformLocation = gl.getUniformLocation(program, 'lightDirVec');
+		g_LightDirVecUniformLocation = gl.getUniformLocation(program, 'vLightDir');
 
 		// Init Model Transform & Camera View & Projection Matrix
-		g_ProjectionMatUniformLocation = gl.getUniformLocation(program, 'projectionMat');
-		g_CameraViewMatUniformLocation = gl.getUniformLocation(program, 'cameraViewMat');
-		g_ModelMatUniformLocation = gl.getUniformLocation(program, 'modelTransMat');
+		g_ProjectionMatUniformLocation = gl.getUniformLocation(program, 'mProjection');
+		g_CameraViewMatUniformLocation = gl.getUniformLocation(program, 'mCameraView');
+		g_ModelMatUniformLocation = gl.getUniformLocation(program, 'mModelTrans');
 
 		var projectionMatrix = mat4.perspective(45.0, g_CanvasAspectRatio, 0.1, 100.0);
 		var cameraViewMatrix = mat4.lookAt([0, 0, 2.8], [0, 0, 0], [0, 1, 0]);
@@ -155,9 +156,12 @@ require(["jquery", "gl-matrix", "modernizr", "model", "shader", "timer"], functi
 		// Use Program
 		g_WebGLContext.useProgram(g_Shader.getGLProgramID());
 		
+		// Clear the Buffer
+		g_WebGLContext.clear(g_WebGLContext.COLOR_BUFFER_BIT | g_WebGLContext.DEPTH_BUFFER_BIT);
+
 		// Light Direction Vector	
 		var lightDirVec = vec3.create([0, 0, 1]);
-		mat4.multiplyVec3(mat4.rotateX(mat4.identity(), Math.sin(g_AnimationKey * Math.PI * 8.0) * Math.PI * 0.5), lightDirVec);	
+		mat4.multiplyVec3(mat4.rotateX(mat4.identity(), Math.sin(g_AnimationKey * Math.PI * 8.0) * Math.PI * 0.5), lightDirVec);
 		gl_WebGLContext.uniform3fv(g_LightDirVecUniformLocation, lightDirVec);
 
 		// Object Transform Matrix
@@ -170,7 +174,7 @@ require(["jquery", "gl-matrix", "modernizr", "model", "shader", "timer"], functi
     	
 		// Improve disabling unused attribute slots
 		var count = g_WebGLContext.getParameter(g_WebGLContext.MAX_VERTEX_ATTRIBS);
-		for (var i = 0; i < count; i++) {
+		for (var i = 0; i < count; ++i) {
 			g_WebGLContext.disableVertexAttribArray(i);
 		}
 
@@ -197,8 +201,6 @@ require(["jquery", "gl-matrix", "modernizr", "model", "shader", "timer"], functi
 				g_WebGLContext.enableVertexAttribArray(loc);
 				g_WebGLContext.vertexAttribPointer(loc, attrs[i].size, g_WebGLContext.FLOAT, false, attrs[i].stride, attrs[i].offset);
 			}
-
-			// offset += attr[i].size * 4;
 		}
     
 		if (g_IndexBufferObject) {
@@ -215,7 +217,7 @@ require(["jquery", "gl-matrix", "modernizr", "model", "shader", "timer"], functi
 			g_Shader = shader;
 			
 			// 初始化场景
-			initScene();
+			initScene(g_WebGLContext);
 		}
 
 		g_Model = model;
